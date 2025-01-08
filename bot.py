b32f5e30311f9f653b2908ccb2d0b41639bb7b81
a1864660 <a1864660@adelaide.edu.au>
2025-01-08 19:40:53 +1030

    added a function to get the string to ping the author of a message, a $echo, and the ability to log message edits and deletions.

--- bot.py
+++ bot.py
@@ -5,6 +5,10 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+# Returns the string to ping the author of a message.
+def ping_author(message):
+    return "<@"+str(message.author.id)+">"
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
@@ -17,6 +21,32 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
+# $Echo: Repeat the message sent by the user.
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$echo'):
+        await message.channel.send(message.content[5:])
+
+# If a message is edited, log the initial message to the same channel.
+@client.event
+async def on_message_edit(before, after):
+    if before.author == client.user:
+        return
+
+    # Remove backticks to prevent formatting issues. Doesn't account for escaped backticks.
+    stripped_message = before.content.replace("`", "")
+    await before.channel.send(ping_author(before) + " just edited their message! Previous Message: ```"+stripped_message+"```")
+
+# If a message gets deleted, log the deleted message to the same channel.
+@client.event
+async def on_message_delete(message):
+    # Remove backticks to prevent formatting issues. Doesn't account for escaped backticks.
+    stripped_message = message.content.replace("`", "")
+    await message.channel.send(ping_author(message) + " just had a message deleted! Previous Message: ```"+stripped_message+"```")
+ 
 @client.event
 async def on_message(message):
     if message.author == client.user:
